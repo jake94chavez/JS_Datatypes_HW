f0c7e44b1cd120aeb97ee1f273d1643154fd7440
Jake Chavez <jake94chavez@gmail.com>
2017-12-11 19:29:26 -0600

    1st commit - selecting datatypes

---
+++ answers.js
@@ -0,0 +1,8 @@
+// Selecting Datatypes
+
+// 1. I would use a boolean. lightOn = true; for example.
+// 2. String. email = "user.email@website.com"
+// 3. Object. const spaceship = {hull: carbon fiber, weapons: "laser blasters", tools: ["tractor beam", "warp drive"]}
+// 4. Array. const students = ["Jake", "Other Student", "etc.", "etc."]
+// 5. Array of objects. const students = [{name: "Jake", location: "bar"}, {name: "Other Student", location:"home"}]
+// 6. Array of objects with a nested array. const students = [{name: "Jake", location: "bar", favShows: ["Game of Thrones", "Adventure Time", "Could keep listing shows like this"]}, {name: "Other Student", location:"home" favShows: ["The Voice", "Insert popular television program here"]}]
