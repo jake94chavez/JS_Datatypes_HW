1097e13e3df264d557559cde8160b8bac90bacf0
Jake Chavez <jake94chavez@gmail.com>
2017-12-11 19:35:20 -0600

    2nd commit - created some data structures and accessed some data

--- answers.js
+++ answers.js
@@ -6,3 +6,19 @@
 // 4. Array. const students = ["Jake", "Other Student", "etc.", "etc."]
 // 5. Array of objects. const students = [{name: "Jake", location: "bar"}, {name: "Other Student", location:"home"}]
 // 6. Array of objects with a nested array. const students = [{name: "Jake", location: "bar", favShows: ["Game of Thrones", "Adventure Time", "Could keep listing shows like this"]}, {name: "Other Student", location:"home" favShows: ["The Voice", "Insert popular television program here"]}]
+
+
+// Take It Easy
+
+const rainbow = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"]
+
+rainbow[4] //would access blue
+
+const jake = {
+	favFood: "Spaghetti and Meatballs",
+	hobby: "Watching television",
+	town: "Cedar Park",
+	favDataType: "Objects",
+}
+
+jake.hobby;
